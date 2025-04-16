782be2a3dc243f4dcb01758371bd2d150027e33b
Felipe Minuzzi <feminuzzi@hotmail.com>
2025-04-16 16:33:10 -0300

    fix figure output

--- src/symb_ocean.py
+++ src/symb_ocean.py
@@ -4,9 +4,18 @@ import pandas as pd
 from pysr import PySRRegressor
 import matplotlib.pyplot as plt
 
+def plot_results(x,y1,y2, x_name, y_name):
+    plt.figure()
+    plt.plot(x, y1,  'r-', label = 'ERA5 H_s')
+    plt.plot(x, y2, 'k--', label = 'PySR H_s')
+    plt.ylabel(x_name)
+    plt.xlabel(y_name)    
+    plt.legend()
+    plt.show()
 
 path            = './data/processed/era5_structured_dataset.csv'
 df              = pd.read_csv(path)
+df['Time']      = pd.to_datetime(df['Time'])
 test_set        = df[-(59*24):].copy().reset_index(drop=True)
 train_set       = df[-(59*24 + (366*24)):-(59*24)].copy().reset_index(drop=True)
 
@@ -37,9 +46,5 @@ print(model)
 y2 = model.predict(X)
 ti = train_set['Time'].values
 
-plt.plot(y,  'r-', label = 'real')
-plt.plot(y2, 'k--', label = 'predicted')
-plt.legend()
-plt.show()
-
+plot_results(ti, y, y2, 'Data', 'Wave height (Hs)')
 breakpoint()
